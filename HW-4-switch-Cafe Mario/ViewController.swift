59a54d322a103df0ee891a9d751be9b30ca96956
Artem Galay <iostema@mail.ru>
2022-08-14 18:36:41 +0300

    Initial Commit

---
+++ HW-4-switch-Cafe Mario/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW-4-switch-Cafe Mario
+//
+//  Created by Артем Галай on 14.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
